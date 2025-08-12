a07bf391d2a78a927a5a700de30272a194857431
Shahin Mammadov <shahinmv9@gmail.com>
2025-08-12 18:31:42 +0400

    message history

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://deltalens-back-production.up.railway.app';
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://deltalens-back-production.up.railway.app';
+// const API_BASE_URL = 'http://localhost:8000';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
